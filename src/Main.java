02f4c149b6abf71b3c0dbb8680f8fe63381712ae
soonberry <shuwenli89@126.com>
2015-01-28 23:56:35 +0800

    for the second test

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is what I use for Pos System on Git.");
+        System.out.println("test a plus");
     }
 }
