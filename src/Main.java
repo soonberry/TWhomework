f16792503e80db517909e1eedfbf8f534662c347
soonberry <shuwenli89@126.com>
2015-01-28 23:37:12 +0800

    for a test

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("This is what I use for Pos System on Git.");
     }
 }
